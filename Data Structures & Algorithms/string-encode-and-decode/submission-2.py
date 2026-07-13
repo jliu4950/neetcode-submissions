30bc90a51b901e3932cfdf0435fbb38d3c814d5a
Jliu <118786103+jliu4950@users.noreply.github.com>
2026-07-13 19:55:12 +0800

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res=""
+        
+        for i in strs:
+            res=res+str(len(i))+"#"+i #字符串的长度可能超过一位数
+        
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        # if s=="":
+        #     return []
+
+        res=[]
+
+        pos=0
+
+        while pos<len(s):            
+            strlen=""
+            nums=["0","1","2","3","4","5","6","7","8","9"]
+            while s[pos] in nums:
+                strlen=strlen+s[pos]
+                pos+=1
+            
+            pos+=1 # 跳过#
+
+            count=int(strlen)
+
+            sub=s[pos:pos+count]
+            res.append(sub)
+
+            pos+=count
+        
+        return res
