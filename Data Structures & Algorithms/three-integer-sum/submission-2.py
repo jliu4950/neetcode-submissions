dffdbc494ef96acb9730d10162bcc9f6cc080ee1
Jliu <118786103+jliu4950@users.noreply.github.com>
2026-07-15 15:50:19 +0800

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        nums.sort()
+
+        print(nums)
+
+        i=0
+
+        while i<len(nums):
+            if nums[i]>0:
+                break
+            while i>0 and i <len(nums) and nums[i]==nums[i-1]:
+                i+=1
+            #print(i)
+            j=i+1 
+            k=len(nums)-1
+            while j<k:
+                total=nums[j]+nums[k]+nums[i]
+                # print(f"i:{i},j:{j},k:{k},total:{total}")
+                # print(f"i:{nums[i]},j:{nums[j]},k:{nums[k]}")
+
+                if total==0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    j+=1
+                    k-=1
+                    #找到正确答案后，把j,k重复值去掉
+                    while j<k and nums[j]==nums[j-1]:
+                        j+=1
+                    while j<k and nums[k]==nums[k+1]:
+                        k-=1
+                elif total<0:
+                    j+=1
+                elif total>0:
+                    k-=1
+
+            i+=1
+        return res
