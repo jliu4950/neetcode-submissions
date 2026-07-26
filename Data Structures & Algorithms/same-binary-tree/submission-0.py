34ba4af243b29af38d791d79cdc086c12520f283
Jliu <118786103+jliu4950@users.noreply.github.com>
2026-07-26 18:15:54 +0800

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and q:
+            return False
+        elif not q and p:
+            return False
+        elif not p and not q:
+            return True
+        
+        left=self.isSameTree(p.left,q.left)
+        right=self.isSameTree(p.right,q.right)
+
+        return left and right and p.val == q.val
+        
